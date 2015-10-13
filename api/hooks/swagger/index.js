4849e486e1ab3cec32d2f87ed9c0ca3f61ebffe4
Alexis Hénaut <alexis@henaut.net>
2015-10-13 18:37:00 -0300

    Move config in the hook "default" function, add default pkg values

--- api/hooks/swagger/index.js
+++ api/hooks/swagger/index.js
@@ -4,8 +4,34 @@ import Marlinspike from 'marlinspike'
 import xfmr from '../../../lib/xfmr'
 
 class Swagger extends Marlinspike {
+
+  defaults (overrides) {
+    return {
+      'swagger': {
+        pkg: {
+          name: 'No package information',
+          description: 'You should set sails.config.swagger.ui.url to retrieve the content of the package.json file',
+          version: '0.0.0'
+        },
+        ui: {
+          url: 'http://localhost:8080/'
+        }
+      },
+      'routes': {
+        '/swagger/doc': {
+          cors: {
+            origin: 'http://localhost:8080, http://online.swagger.io',
+            methods: 'GET,OPTIONS,HEAD'
+          },
+          controller: 'SwaggerController',
+          action: 'doc'
+        }
+      }
+    };
+  }
+
   constructor (sails) {
-    super(sails, module)
+    super(sails, module);
   }
 
   initialize (next) {
