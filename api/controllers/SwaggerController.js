dbbe91a9331fe5157632c648714196d8d0c34fa1
ggordon <ggg_gilroy@yahoo.com>
2016-03-28 17:09:56 -0300

    doc query parameter issue fixed

--- api/controllers/SwaggerController.js
+++ api/controllers/SwaggerController.js
@@ -1,12 +1,12 @@
 const SwaggerController = {
-  doc (req, res) {
-    res.status(200).jsonx(sails.hooks.swagger.doc)
-  },
+    doc(req, res) {
+        res.status(200).jsonx(sails.hooks.swagger.doc)
+    },
 
-  ui (req, res) {
-    let docUrl = req.protocol + '://' + req.get('Host') + '/swagger/doc'
-    res.redirect(sails.config.swagger.ui.url + '?url=' + encodeURIComponent(docUrl))
-  }
+    ui(req, res) {
+        let docUrl = req.protocol + '://' + req.get('Host') + '/swagger/doc'
+        res.redirect(sails.config.swagger.ui.url + '?doc=' + encodeURIComponent(docUrl))
+    }
 }
 
 export default SwaggerController
